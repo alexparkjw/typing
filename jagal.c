17275a30e448a80cc302024270101360c685df00
alex jw park <alex@alexs-MacBook.local>
2019-05-28 01:02:19 +0900

    jagal.c update2

--- jagal.c
+++ jagal.c
@@ -2,35 +2,41 @@
 #include<stdlib.h>
 #include<math.h>
 
+int rotate(int num_a, int n, int k) {
+    int num_c = num_a, num_b, flag;
+    for(int i=0; i<k; i++) {
+        flag = num_c & (int)pow(2, n-1);
+        num_b = num_c << 1; 
+        num_b += flag;
+        num_c = num_c ^ num_b;
+    }
+    return num_c;
+}
+
+
 int main(void) {
     int n, k;
     scanf("%d %d", &n, &k);
     char *input = (char *)malloc(sizeof(char)*(n+1));
     scanf(" %s", input);
 
-    int num = 0, num1 = 0,  num2 = 0;
-    int flag = 0,  out;
-
-    if(input[0] == 'W') flag = 1;
-
+    // convert input to num;
+    int num;
     for(int i=0; i<n; i++) {
         if(input[i] == 'W')
             num += (int)pow(2, n-i-1);
     }
    
-    num1 = num;
-    for(int i=0; i<k; i++) {
-        num2 = num1 << 1 ;       
-        num2 += flag;
-        num1 = num1 ^ num2;
-    } 
-
-    num2 = num1 ^ num;
-
-    printf("%d\n", num2);
+    // counting
+    int count = 1;
+    for(int i=0, size=(int)pow(2, n-1); i<size; i++) {
+        if(rotate(i, n, k) == rotate(num, n, k)) 
+            count++;
+    }
+    printf("%d\n", count);
 
     /* for(int i = 31; i >=0; i--) { */
-    /*     out = num2 >> i; */
+    /*     out = num_c >> i; */
     /*     if(i < n) { */
     /*         if ( out & 1) */ 
     /*             printf("W"); */
