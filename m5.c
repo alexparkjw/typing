20bb07eb169965eabba109028d3593db624c349b
Alex <alexparkjw@gmail.com>
2020-02-01 22:48:56 +0900

    m5.c 2

--- m5.c
+++ m5.c
@@ -19,7 +19,7 @@ int main(void) {
     for(int i=0; i<R; i++) dp[i] = (int*)malloc(sizeof(int)*C);
 
     // 배열 -> 포인터
-    memCpy(dp, arr, sizeof(int**));
+    memcpy(dp, arr, sizeof(int**));
 
     // 로컬포인터 -> 글로벌포인터
     gp = dp;
@@ -36,13 +36,6 @@ int main(void) {
 
     return EXIT_SUCCESS;
 }
-
-void memCpy(void *dest, void *src, size_t size) { 
-    char *c_src = *(char **)src; 
-    char *c_dest = *(char **)dest; 
-    for (int i=0; i<size; i++) 
-        c_dest[i] = c_src[i]; 
-} 
 
 void print(int **a, int r, int c) {
     for(int i=0; i<r; i++) {
