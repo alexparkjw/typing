3fbf1309220af56ec23f59e8d51224fbe60d7f0a
Alex <alexparkjw@gmail.com>
2020-02-01 23:00:05 +0900

    m5.c 3

--- m5.c
+++ m5.c
@@ -5,29 +5,27 @@
 #define R 3
 #define C 3
 
-void memCpy(void *dest, void *src, size_t size);
-void print(int **a, int r, int c);
+void print(int r, int c, int a[r][c]);
 int **gp = NULL;
 
 int main(void) {
     // 배열 
     int arr[R][C] = { {1, 2, 3}, {4, 5, 6}, {7,8,9} };
-    print(arr, R, C);
 
     // 포인터
     int **dp = (int**)malloc(sizeof(int*)*R);
     for(int i=0; i<R; i++) dp[i] = (int*)malloc(sizeof(int)*C);
 
     // 배열 -> 포인터
-    memcpy(dp, arr, sizeof(int**));
+    memcpy(dp, arr, sizeof(int**)*R*C);
 
     // 로컬포인터 -> 글로벌포인터
     gp = dp;
     
     //확인용출력
-    print(arr, R, C);
-    print(dp, R, C);
-    print(gp, R, C);
+    print(R, C, arr);
+    print(R, C, dp);
+    print(R, C, gp);
 
     // 메모리 해제
     for(int i=0; i<R; i++)
@@ -37,7 +35,7 @@ int main(void) {
     return EXIT_SUCCESS;
 }
 
-void print(int **a, int r, int c) {
+void print(int r, int c, int a[r][c]) {
     for(int i=0; i<r; i++) {
         for(int j=0; j<c; j++) 
             printf("%d ", a[i][j]);
