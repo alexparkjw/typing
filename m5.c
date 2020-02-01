25360b1dc58e88328a6a49690bfc7a96d9d41855
Alex <alexparkjw@gmail.com>
2020-02-02 01:20:41 +0900

    add m5.c 5

--- m5.c
+++ m5.c
@@ -5,45 +5,69 @@
 #define R 3
 #define C 3
 
-void print(int r, int c, void *vp);
+void print1(int r, int c, void *vp);
+void print2(int r, int c, void *vp);
 int **gp = NULL;
 
 int main(void) {
     // 배열 
-    int arr[R][C] = { {1, 2, 3}, {4, 5, 6}, {7,8,9} };
+    int arr[R][C] = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
+    print2(R, C, arr);
 
     // 포인터
-    int **dp = NULL;
-    dp  = (int**)malloc(sizeof(int*)*R);
+    int **dp = (int**)malloc(sizeof(int*)*R);
     for(int i=0; i<R; i++) 
         dp[i] = (int*)malloc(sizeof(int)*C);
 
     // 배열 -> 포인터
-    memcpy(dp, arr, sizeof(int**)*R*C);
+    for(int i=0; i<R; i++) {
+        for(int j=0; j<C; j++) {
+            dp[i][j] = arr[i][j];
+        }
+    }
 
     // 로컬포인터 -> 글로벌포인터
     gp = dp;
     
-    //확인용출력
-    print(R, C, arr);
-    print(R, C, dp);
-    print(R, C, gp);
+    //확인용 출력1
+    print1(R, C, dp);
+    print1(R, C, gp);
+
+    // 배열 -> 포인터 맴카피로....하면
+    memcpy(dp, arr, sizeof(int**)*R*C);
+
+    // 확인용 출력 2
+    print2(R, C, dp);
+    print2(R, C, gp);
 
     // 메모리 해제
-    /* for(int i=0; i<R; i++) */ 
-        /* free(dp[i]); */
-    free(dp);
+    for(int i=0; i<R; i++)  {
+        if(dp[i] != NULL)
+            ;
+    }
+    if(dp != NULL)
+        free(dp);
 
     return EXIT_SUCCESS;
 }
 
-void print(int r, int c, void *vp) {
-    int (*a)[c] = vp; 
+void print1(int r, int c, void *vp) {
+    int **a = vp; 
     for(int i=0; i<r; i++) {
         for(int j=0; j<c; j++) 
-            printf("%d ", *(*(a+i)+j));
+            printf("%d ", a[i][j]);
         printf("\n");
     }
     printf("\n");
 }
 
+
+void print2(int r, int c, void *vp) {
+    int (*a)[c] = vp; 
+    for(int i=0; i<r; i++) {
+        for(int j=0; j<c; j++) 
+            printf("%d ", a[i][j]);
+        printf("\n");
+    }
+    printf("\n");
+}
